435c5224ffbe503fd768e9c319900571b7acedd2
mr-indian <harshsingh55818@gmail.com>
2023-03-09 00:53:02 +0530

    add-to-cart design

--- src/App.js
+++ src/App.js
@@ -14,3 +14,12 @@ export default App;
 
 //pehle hum component ko directly use krte the but
 // Redux mai pehle uss component ko ek container mai dalo phir uss container ko App.js mai rkho
+
+
+//git branch 
+//git checkout -b dev         branch dev name ka
+//git add .
+//git commit -m "add-to-cart design"
+//git push
+//git push --set-upstream origin dev
+//username/password
